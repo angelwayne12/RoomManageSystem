3ef634c7c5c1cc20d5d7aefbbfee4bd295b1e08d
Agile <Agile@DESKTOP-U3H3OL9>
2024-04-05 20:21:31 +0800

    Add project files.

---
+++ RoomManageSystemBL/Room.cs
@@ -0,0 +1,12 @@
+﻿namespace RoomManageSystemBL
+{
+    public class Room
+    {
+        public string roomNo;
+        public string name;
+        public string phonenum;
+        public string numRent;
+        public string pets;
+        public string Ac;
+    }
+}
